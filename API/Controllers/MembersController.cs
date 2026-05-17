17bb931eadf43edc04975cfbe3ca99260ea069a9
stevhook <stevhook@proton.me>
2026-05-17 21:09:29 +0100

    feat(authentication): configure JWT authentication and update MembersController for authorisation

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,8 @@ namespace API.Controllers
 
             return members;
         }
-
+        
+        [Authorize]
         [HttpGet("{id}")] // api/members/test1-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
