db597d31b2d65ce6229300ff1e264e6830c2b5fe
stevhook <stevhook@proton.me>
2026-05-17 20:14:01 +0100

    feat(authentication): implement login functionality and add LoginDto for user authentication

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -13,7 +13,6 @@ public class AccountController(AppDbContext context) : BaseApiController
     [HttpPost("register")] // api/account/register
     public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
-
         if (await EmailExists(registerDto.Email)) return BadRequest("Email already registered");
 
         using var hmac = new HMACSHA512();
@@ -31,6 +30,26 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         return user;
     }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+        if (user == null) return Unauthorized("Invalid email address");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
 
     private async Task<bool> EmailExists(string email)
     {
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,13 +5,13 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public string DisplayName { get; set; }
+    public string DisplayName { get; set; } = "";
 
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = "";
 
     [Required]
     [MinLength(4)]
-    public string Password { get; set; }
+    public string Password { get; set; } = "";
 }
