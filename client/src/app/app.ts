0c6746901e5253cb901220603f864a876bc0f21b
stevhook <stevhook@proton.me>
2026-05-19 21:42:54 +0100

    feat(register): implement registration component with form and validation feat(home): integrate registration component and manage display logic refactor(app): update app structure to utilize new home and registration components

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,10 +3,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,7 +16,7 @@ export class App implements OnInit{
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = 'Dating App';
-  protected members = signal<any>([])
+  protected members = signal<User[]>([])
 
   async ngOnInit() {
     this.members.set(await this.getMembers())
@@ -30,7 +32,7 @@ export class App implements OnInit{
   
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
       throw error;
---
+++ client/src/features/accounts/register/register.ts
@@ -0,0 +1,23 @@
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisterCreds, User } from '../../../types/user';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  membersFromHome = input.required<User[]>();
+  cancelRegister = output<boolean>();
+  protected creds = {} as RegisterCreds;
+
+  register() {
+    console.log(this.creds)
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
