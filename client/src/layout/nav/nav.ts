86b2b6c67148c2ca7bc6980af3e85323ac89b9ab
stevhook <stevhook@proton.me>
2026-05-19 20:54:45 +0100

    feat(account): integrate user state management in navigation and app components

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -9,7 +9,7 @@ import { AccountService } from '../../core/services/account-service';
   styleUrl: './nav.css',
 })
 export class Nav {
-  private accountService = inject(AccountService)
+  protected accountService = inject(AccountService)
   protected creds: any = {};
   protected loggedIn = signal(false);
 
@@ -17,7 +17,6 @@ export class Nav {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result);
-        this.loggedIn.set(true)
         this.creds = {};
       },
       error: error => alert(error.message),
@@ -25,6 +24,6 @@ export class Nav {
   }
 
   logout() {
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
