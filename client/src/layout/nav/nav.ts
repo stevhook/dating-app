bdb5f5f6c18c4610b5707960fba6dbd03aebe971
stevhook <stevhook@proton.me>
2026-05-29 23:21:24 +0100

    feat(navigation): implement routing and update navigation links for members, lists, and messages

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,29 +1,31 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
-  protected accountService = inject(AccountService)
+  protected accountService = inject(AccountService);
+  private router = inject(Router);
   protected creds: any = {};
-  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
-        console.log(result);
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message),
+      error: error => alert(error.message)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
