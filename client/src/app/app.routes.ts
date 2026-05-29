9000110813d5829fd7b42e87944a22973e0ca0b5
stevhook <stevhook@proton.me>
2026-05-29 23:45:43 +0100

    feat(routes): restructure routing to group protected routes under a parent with authGuard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,10 +7,17 @@ import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guards/auth-guard';
 
 export const routes: Routes = [
-    {path: '', component: Home},
-    {path: 'members', component: MemberList, canActivate: [authGuard]},
-    {path: 'members/:id', component: MemberDetailed, canActivate: [authGuard]},
-    {path: 'lists', component: Lists, canActivate: [authGuard]},
-    {path: 'messages', component: Messages, canActivate: [authGuard]},
-    {path: '**', component: Home},
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberList },
+            { path: 'members/:id', component: MemberDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },       
+        ]
+    },
+    { path: '**', component: Home },
 ];
