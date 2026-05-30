8add285afdec5eeaea5edd0084e72cc4238ec45c
stevhook <stevhook@proton.me>
2026-05-30 23:11:39 +0100

    feat(controller): refactor MembersController to use IMemberRepository for member data retrieval

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,30 +1,33 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class MembersController(AppDbContext context) : BaseApiController
+    [Authorize]
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet] // api/members
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
-
-            return members;
+            return Ok(await memberRepository.GetMembersAsync());
         }
         
-        [Authorize]
-        [HttpGet("{id}")] // api/members/test1-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        [HttpGet("{id}")] // api/members/test-id
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
 
             if (member == null) return NotFound();
 
             return member;
+        }
+
+        [HttpGet("{id}/photos")] // api/test-id/photos
+        public async Task<ActionResult<Member>> GetPhotos(string id)
+        {
+            return Ok(await memberRepository.GetPhotosForMemberAsync(id));
         }
     }
 }
