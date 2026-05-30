671873dff98797c6da33505e02e217bbb29476a2
stevhook <stevhook@proton.me>
2026-05-30 22:54:59 +0100

    feat(repository): implement MemberRepository and IMemberRepository interface for member data management

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
