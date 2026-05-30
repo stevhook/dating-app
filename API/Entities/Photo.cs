649ee84d89fa7c62ce150e7a7b0fbce9a4430eea
stevhook <stevhook@proton.me>
2026-05-30 19:01:52 +0100

    feat(database): add Member and Photo entities with relationships to AppUser

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public string? PublicId { get; set; }
+    
+    // Navigation property
+    public Member Member { get; set; } = null!;
+    
+}
