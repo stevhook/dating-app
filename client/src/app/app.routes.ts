1c83217d8086166326030b77c7753c5c97f9ff57
stevhook <stevhook@proton.me>
2026-05-31 16:15:13 +0100

    feat: add member resolver and update routing for member detailed view

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { ServerError } from '../shared/errors/server-error/server-error';
 import { MemberProfile } from '../features/members/member-profile/member-profile';
 import { MemberMessages } from '../features/members/member-messages/member-messages';
 import { MemberPhotos } from '../features/members/member-photos/member-photos';
+import { memberResolver } from '../features/members/member-resolver';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -22,6 +23,8 @@ export const routes: Routes = [
             { path: 'members', component: MemberList },
             { 
                 path: 'members/:id', 
+                resolve: {member: memberResolver},
+                runGuardsAndResolvers: 'always',
                 component: MemberDetailed,
                 children: [
                     { path: '', redirectTo: 'profile', pathMatch: 'full'},
