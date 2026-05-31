2be854d4d4033e8f801539099099e6b4357b6bc7
stevhook <stevhook@proton.me>
2026-05-31 15:20:15 +0100

    feat: add JWT interceptor for authorization and clean up member service

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { AccountService } from './account-service';
 import { Member } from '../../types/member';
 
 @Injectable({
@@ -9,22 +8,13 @@ import { Member } from '../../types/member';
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService)
   private baseUrl = environment.apiUrl;
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + 'members/' + id, this.getHttpOptions());
-  }
-
-  private getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
   }
 }
